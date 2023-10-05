cf1c6fe321506f9d9ce9611f4ececb610c6d907f
XuandongZhao <csxuandongzhao@gmail.com>
2023-10-05 11:16:35 -0700

    add detect attack results in demo.ipynb

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
@@ -426,11 +426,11 @@ class StableDiffusionPipeline(DiffusionPipeline, TextualInversionLoaderMixin, Lo
         return image, has_nsfw_concept
 
     def decode_latents(self, latents):
-        warnings.warn(
-            "The decode_latents method is deprecated and will be removed in a future version. Please"
-            " use VaeImageProcessor instead",
-            FutureWarning,
-        )
+        # warnings.warn(
+        #     "The decode_latents method is deprecated and will be removed in a future version. Please"
+        #     " use VaeImageProcessor instead",
+        #     FutureWarning,
+        # )
         latents = 1 / self.vae.config.scaling_factor * latents
         image = self.vae.decode(latents, return_dict=False)[0]
         image = (image / 2 + 0.5).clamp(0, 1)
